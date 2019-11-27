01a1c8cc49357528bcaea7e5a983d22a5dd0611a
korrakot1396 <freetoplay.love@hotmail.com>
2019-11-27 16:13:37 +0700

    delete viedo.mp4

--- src/com/gn/App.java
+++ src/com/gn/App.java
@@ -49,7 +49,7 @@ public class App extends Application {
 
         float total = 43; // the difference represents the views not loaded
         increment = 100f / total;
-        
+
 
 
         load("main",     "main");
